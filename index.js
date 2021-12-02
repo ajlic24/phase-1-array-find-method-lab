edfa4df6a3115fad4b5cab0f56ee085b07183694
ajlic24@gmail.com <ajlic24@gmail.com>
2021-12-02 10:27:45 -0500

    commit tree d7ac92df1a47ba54f006c1be6c4321fa104acab8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+function superbowlWin(array) {
+    const result = array.find(item => item.result === "W")
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
+
+// console.log(superbowlWin(record))
